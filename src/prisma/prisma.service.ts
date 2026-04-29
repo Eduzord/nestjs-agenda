fb91c2903c51926fdb2929fc3e6968b6753083ee
Eduardo Oliveira <emoliveira97@gmail.com>
2026-04-28 21:53:26 -0300

    Erros corrigidos, erro de prisma generate

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -6,9 +6,14 @@ import mariadb from 'mariadb';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
     constructor() {
-        
-        const adapter = new PrismaMariaDb(process.env.DATABASE_URL!);
 
+        const adapter = new PrismaMariaDb({
+            host: 'localhost',
+            port: 3306,
+            user: 'root', // Usuário padrão do XAMPP
+            password: '', // Deixe vazio se você não configurou senha no XAMPP
+            database: 'projeto_crud_db', // Coloque o exato nome do seu banco aqui
+        });
         super(adapter);
     }
 
